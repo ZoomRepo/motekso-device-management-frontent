a171002517e5ab874435911acdc53012b034ec9f
Ollie Beumkes <ollybeumkes@hotmail.co.uk>
2023-12-06 22:55:14 +0000

    added home path

--- app/adapters/controllers.py
+++ app/adapters/controllers.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request
 from app.use_cases.example_use_case import ExampleUseCase
 from app.gateways.database import UserRepository
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
+
 user_repository = UserRepository()
 example_use_case = ExampleUseCase(user_repository)
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
+
+# ... other route
 @app.route('/user/<int:user_id>', methods=['GET'])
 def get_user(user_id):
     user = example_use_case.get_user(user_id)
@@ -18,10 +25,6 @@ def create_user():
     email = data.get('email')
     new_user = example_use_case.create_user(username, email)
     return jsonify({"id": new_user.id, "username": new_user.username, "email": new_user.email})
-
-@app.route('/', methods=['GET'])
-def home():
-    return "Welcome to the Flask Clean Architecture Example!"
 
 if __name__ == '__main__':
     app.run(debug=True)
